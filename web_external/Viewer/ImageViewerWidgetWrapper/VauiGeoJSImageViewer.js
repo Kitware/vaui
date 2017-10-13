70fd2e837ba197eeff97da7629cc9e13d50ba578
Matthew Ma <matthew.ma@kitware.com>
2017-10-13 16:26:10 -0400

    Fix few issues that could cause frame, annotation, and slider out of sync
    Also improve annotation loading speed

--- web_external/Viewer/ImageViewerWidgetWrapper/VauiGeoJSImageViewer.js
+++ web_external/Viewer/ImageViewerWidgetWrapper/VauiGeoJSImageViewer.js
@@ -6,15 +6,15 @@ var VauiGeoJSImageViewer = GeojsImageViewerWidget.extend({
     render() {
         GeojsImageViewerWidget.prototype.render.call(this);
         var map = this.viewer;
-        var ids = [this.itemId];
+        var ids = null;
         var siblings = new ItemCollection();
         siblings.pageLimit = 0;
         siblings.fetch({ folderId: this.model.get('folderId') })
             .done(() => {
-                siblings.each((model) => {
-                    ids.push(model.id);
-                });
-                console.log('Got ' + ids.length + ' frame ids');
+                ids = siblings
+                    .toArray()
+                    .filter((itemModel) => itemModel.get('largeImage'))
+                    .map((model) => model.id);
 
                 // change from our default of only allowing to zoom to 1 pixel is 1 pixel
                 // to allow 1 pixel to be 8x8.
@@ -24,10 +24,13 @@ var VauiGeoJSImageViewer = GeojsImageViewerWidget.extend({
                 // create a second osm layer.  We use these as A-B buffers.  We start with
                 // them being identical
                 var l2 = map.createLayer('osm', l1._options);
+                l1.zIndex(9);
+                l1.moveToBottom();
                 var frame = 0;
                 var stopflag = 1;
 
                 var updateFrame = () => {
+                    this.updating = true;
                     return new Promise((resolve, reject) => {
                         map.onIdle(() => {
 
@@ -41,15 +44,13 @@ var VauiGeoJSImageViewer = GeojsImageViewerWidget.extend({
                             map.onIdle(() => {
                                 // onIdle is called when all of our tiles are loaded.  Move the
                                 // back layer to the top and swap our layer references
+                                this._drawAnnotation();
                                 var l1zIndex = l1.zIndex();
-                                l1.zIndex(l2.zIndex());
+                                l1.zIndex(l2.zIndex(), true);
                                 l2.zIndex(l1zIndex);
                                 var ltemp = l1; l1 = l2; l2 = ltemp;
 
-                                map.onIdle(() => {
-                                    this._drawAnnotation(frame);
-                                });
-
+                                this.updating = false;
                                 resolve();
                             });
                         });
@@ -91,25 +92,54 @@ var VauiGeoJSImageViewer = GeojsImageViewerWidget.extend({
                 this.setFrame = (newFrame) => {
                     if (newFrame >= 0
                         && newFrame <= ids.length - 1) {
-                        frame = newFrame;
-                        updateFrame()
-                            .then(() => {
-                                this.trigger('progress', frame, ids.length);
-                            });
+                        if (!this.updating) {
+                            this.pendingFrame = null;
+                            frame = newFrame;
+                            updateFrame()
+                                .then(() => {
+                                    this.trigger('progress', frame, ids.length);
+                                    if (this.pendingFrame) {
+                                        this.setFrame(this.pendingFrame);
+                                    }
+                                });
+                        }
+                        else {
+                            this.pendingFrame = newFrame;
+                        }
                     }
                 }
 
                 this._drawAnnotation = () => {
-                    if (!this.featureFrames) {
+                    if (!this.annotationFrames) {
                         return;
                     }
                     if (this.lastFeatureFrame) {
-                        this.lastFeatureFrame.visible(false);
+                        this.featureLayer.deleteFeature(this.lastFeatureFrame);
                     }
-                    this.lastFeatureFrame = this.featureFrames[frame].visible(true);
-                    console.log("before draw");
+                    var annotationFrame = this.annotationFrames[frame];
+                    var feature = this.featureLayer.createFeature('polygon');
+                    feature.data(annotationFrame.features);
+                    feature.polygon((d) => {
+                        var coord = d.geometry.coordinates[0];
+                        return {
+                            outer: [{ x: coord[0][0], y: coord[0][1] },
+                            { x: coord[1][0], y: coord[1][1] },
+                            { x: coord[2][0], y: coord[2][1] },
+                            { x: coord[3][0], y: coord[3][1] }]
+                        }
+                    });
+                    feature.style({
+                        fill: true,
+                        fillColor: { r: 1.0, g: 0.839, b: 0.439 },
+                        fillOpacity: 0.4,
+                        radius: 5.0,
+                        stroke: true,
+                        strokeColor: { r: 0.851, g: 0.604, b: 0.0 },
+                        strokeWidth: 1.25,
+                        strokeOpacity: 0.8
+                    });
+                    this.lastFeatureFrame = feature;
                     this.viewer.draw();
-                    console.log("after draw");
                 }
 
                 updateFrame();
@@ -119,38 +149,7 @@ var VauiGeoJSImageViewer = GeojsImageViewerWidget.extend({
             });
 
         this.setAnnotationFrames = (annotationFrames) => {
-            var featureFrames = [];
-            this.featureFrames = featureFrames;
-            console.log("start");
-            annotationFrames.forEach((annotationFrame) => {
-                var feature = this.featureLayer.createFeature('polygon');
-                feature.data(annotationFrame.features);
-                feature.polygon((d) => {
-                    var coord = d.geometry.coordinates[0];
-                    return {
-                        outer: [{ x: coord[0][0], y: coord[0][1] },
-                        { x: coord[1][0], y: coord[1][1] },
-                        { x: coord[2][0], y: coord[2][1] },
-                        { x: coord[3][0], y: coord[3][1] }]
-                    }
-                });
-                feature.style({
-                    fill: true,
-                    fillColor: { r: 1.0, g: 0.839, b: 0.439 },
-                    fillOpacity: 0.4,
-                    radius: 5.0,
-                    stroke: true,
-                    strokeColor: { r: 0.851, g: 0.604, b: 0.0 },
-                    strokeWidth: 1.25,
-                    strokeOpacity: 0.8
-                })
-                feature.visible(false);
-                featureFrames.push(feature);
-            });
-            console.log("end");
-            if (this._drawAnnotation) {
-                this._drawAnnotation();
-            }
+            this.annotationFrames = annotationFrames;
         }
     }
 });
